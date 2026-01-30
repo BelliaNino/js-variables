fea24b2fcc5cc9aef1118c083240aeae3624b7f1
BelliaNino <bellianino1996@gmail.com>
2026-01-30 13:52:29 +0100

    fix ex 4. to "strings"

--- ex-js-variables (1)/02-strings/script.js
+++ ex-js-variables (1)/02-strings/script.js
@@ -20,7 +20,7 @@ console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
 
 
